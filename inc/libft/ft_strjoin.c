a18017dffe1b3e6127d990100498e5f741e189ec
jverdu-r <102660580+jverdu-r@users.noreply.github.com>
2022-12-27 11:41:30 +0100

    Add files via upload

--- inc/libft/ft_strjoin.c
+++ inc/libft/ft_strjoin.c
@@ -20,6 +20,9 @@ char	*ft_strjoin(char *s1, char *s2)
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
+	ptr = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (ptr == NULL)
+		return (NULL);
 	a = 0;
 	b = 0;
 	if (s1)
